8f6a26b226ce0b14a84000a9f293aab517293783
Aaron J. Kan <ajk260@cheese.cs.rutgers.edu>
2020-11-29 13:24:48 -0500

    committing one line

--- Asst2.c
+++ Asst2.c
@@ -713,7 +713,7 @@ int main(int argc, char** argv) {
     for(dsPtr = *headPtr; dsPtr->next != NULL; dsPtr = dsPtr->next) {
         for(dsPtr2 = dsPtr->next; dsPtr2!=NULL; dsPtr2 = dsPtr2->next){
             printf("Attempting JSD on: %s AND \t%s\n", dsPtr->path, dsPtr2->path);
-            //double jsd = jensenShannonDist(dsPtr, dsPtr2);
+            double jsd = jensenShannonDist(dsPtr, dsPtr2);
             //printf("%f %s %s\n", jsd ,dsPtr->path, dsPtr2->path);
         }
     } 
